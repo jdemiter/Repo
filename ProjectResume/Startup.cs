9fae50be72d1a57e6bdc317e6c1c56d242a10710
jdemiter <jdemiter@W766297>
2017-03-09 20:56:00 -0700

    I don't know if I did any changes but I'm saving it

--- ProjectResume/Startup.cs
+++ ProjectResume/Startup.cs
@@ -92,6 +92,8 @@ namespace ProjectResume
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            
         }
     }
 }
