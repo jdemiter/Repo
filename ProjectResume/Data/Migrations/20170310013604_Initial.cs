a8fb4a39c6309fdfc71d3d336f582156254b06b3
jdemiter <jdemiter@W766297>
2017-03-09 18:43:48 -0700

    Initial

---
+++ ProjectResume/Data/Migrations/20170310013604_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ProjectResume.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkExperience",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Employer = table.Column<string>(nullable: true),
+                    EmploymentDate = table.Column<DateTime>(nullable: false),
+                    JobDuties = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkExperience", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkExperience");
+        }
+    }
+}
---
+++ ProjectResume/Models/WorkExperience.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectResume.Models
+{
+    public class WorkExperience
+    {
+        public int ID { get; set; }
+        public string Employer { get; set; }
+        public string JobDuties { get; set; }
+        public DateTime EmploymentDate { get; set; }
+    }
+}
